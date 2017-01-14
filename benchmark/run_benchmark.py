cbe2eb6dbc237e10de5447b13309b622484d56d1
Kevin Schellenberg <wikkiewikkie@gmail.com>
2017-01-14 09:17:14 -0500

    update benchmark script

--- benchmark/run_benchmark.py
+++ benchmark/run_benchmark.py
@@ -6,6 +6,7 @@ import os
 
 if __name__ == "__main__":
 
+    print("Creating test files...")
     file_names = []
     for num in range(1, 1001):
         bates_number = str(num).zfill(6)
@@ -17,6 +18,7 @@ if __name__ == "__main__":
         with open(file_name, "wb") as out_file:
             out_file.write(p.read())
 
+    print("Starting Benchmark...")
     start = time.perf_counter()
 
     m = Marisol("TESTOUT", 6, 1)
@@ -27,9 +29,14 @@ if __name__ == "__main__":
         d.save()
 
     end = time.perf_counter()
+    print("Benchmark Complete.")
 
+    print("{} seconds elapsed.".format(end-start))
+    print("{} pages per second.".format(1000/(end-start)))
+
+    print("Cleaning up...")
     for file_name in os.listdir():
-        if file_name[:-4] == ".pdf":
+        if file_name[-4:] == ".pdf":
             os.remove(file_name)
 
-    print(end-start)
+
